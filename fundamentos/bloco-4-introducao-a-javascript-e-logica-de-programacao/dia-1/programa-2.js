92807df8e0d052787002c82304bfb02b6c217620
Mychel Albuquerque <mychelalbu@gmail.com>
2022-06-21 15:02:41 -0300

    feat: adiciona programa 2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1/programa-2.js
@@ -0,0 +1,17 @@
+let a = 7
+let b = 17;
+let maior = 'Números iguais';
+
+
+console.log('-------------------------');
+console.log('# Programa 2 #');
+console.log('-------------------------');
+if (a!=b){
+  if (a>b) {
+    maior = `O maior é ${a}`;
+  } else {
+    maior = `O maior é ${b}`;
+  }
+} 
+console.log(maior);
+console.log('-------------------------');
