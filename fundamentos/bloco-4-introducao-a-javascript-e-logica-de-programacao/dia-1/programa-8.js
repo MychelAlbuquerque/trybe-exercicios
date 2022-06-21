042a2e060e1292533fc32596aadbe50c82d48db9
Mychel Albuquerque <mychelalbu@gmail.com>
2022-06-21 16:57:21 -0300

    feat: adiciona programa 8

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1/programa-8.js
@@ -0,0 +1,11 @@
+const a = -20;
+let maior = 'Zero';
+
+
+console.log('-------------------------');
+console.log('# Programa 8 #');
+console.log('---------------');
+if (a>0) maior = 'Positive'; 
+else if (a<0) maior = 'Negative'
+console.log(maior)
+console.log('-------------------------');
