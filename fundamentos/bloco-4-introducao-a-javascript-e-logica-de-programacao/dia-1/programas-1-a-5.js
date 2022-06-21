77473892639c4001ed0a9fe46a8303f1e3d00156
Mychel Albuquerque <mychelalbu@gmail.com>
2022-06-21 14:17:45 -0300

    feat: adiciona programas de 1 a 5

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1/programas-1-a-5.js
@@ -0,0 +1,14 @@
+let a = 5;
+let b = 7;
+
+
+console.log('-------------------------');
+console.log('# Programas de 1 a 5 #');
+console.log('-------------------------');
+console.log(`Adição: ${a+b}`);
+console.log(`Subtração: ${a-b}`);
+console.log(`Multiplicação: ${a*b}`);
+console.log(`Divisão: ${a/b}`);
+console.log(`Resto: ${a%b}`);
+console.log('-------------------------');
+
