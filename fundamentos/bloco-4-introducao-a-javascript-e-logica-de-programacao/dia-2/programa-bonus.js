96f1659b798e241235df7f3baf45329f5d5d27a5
Mychel Albuquerque <mychelalbu@gmail.com>
2022-06-25 00:05:14 -0300

    feat: adiciona programa bonus

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2/programa-bonus.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let ordem =[];
+
+//for(let index=0;index<numbers.length;index+=1){
+  for(let secondIndex=0; secondIndex<numbers.length; secondIndex+=1){
+    if (numbers[index]> numbers[secondIndex]){ 
+      ordem[index] = numbers[secondIndex];
+    }
+    console.log(ordem);
+  }    
+//}
+
